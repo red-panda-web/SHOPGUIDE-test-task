172029c83472a99c48db57e152511fcc93c5d600
red-panda-web <readex934@gmail.com>
2020-09-27 23:08:50 +0300

    added working tabs

--- script/script.js
+++ script/script.js
@@ -1,4 +1,6 @@
 let count = Math.floor(document.querySelector(".swiper-container").offsetWidth / 250);
+
+//slider
 var mySwiper = new Swiper('.swiper-container', {
 	loop: true,
 	slidesPerView: count,
@@ -9,4 +11,23 @@ var mySwiper = new Swiper('.swiper-container', {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
 	},
+});
+
+//tabs
+let triggers = document.querySelectorAll(".js-trigger"); // считываем все табы
+
+triggers.forEach((item) => {
+	item.addEventListener("click", function () {	//на каждый вешаем событие
+		let id = this.getAttribute("tabindex");	// узнаем какой таб был нажат
+		let content = document.querySelector('.js-content[tabindex="' + id + '"]'); // и какой текст ему соответствует
+
+		let active_trigger = document.querySelector(".js-trigger.active"); // находим активный в данный момент таб
+		let active_content = document.querySelector(".js-content.active"); // и текст
+
+		active_trigger.classList.remove("active");	//переназначаем классы с активного таба и текста на выбранные
+		item.classList.add("active");
+
+		active_content.classList.remove("active");
+		content.classList.add("active");
+	})
 })
