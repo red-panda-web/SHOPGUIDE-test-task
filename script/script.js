522ea2d95193a28b942651316e4b81000ede0b1a
red-panda-web <readex934@gmail.com>
2020-10-04 21:59:40 +0300

    added animation for tabs

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-let count = Math.floor(document.querySelector(".swiper-container").offsetWidth / 250);
+let count = Math.floor(document.querySelector(".swiper-container").offsetWidth / 250); // ширина слайдов 
 
 //slider
 var mySwiper = new Swiper('.swiper-container', {
@@ -15,26 +15,28 @@ var mySwiper = new Swiper('.swiper-container', {
 
 //tabs
 let triggers = document.querySelectorAll(".js-trigger"); // считываем все табы
-let img_arr = ["img/phone2_list.jpg",	// массив изображений
-	"img/phone2_actions.jpg",
-	"img/phone2_subsribtions.jpg",
-	"img/phone2_map.jpg",
-	"img/phone2_favorites.jpg"];
 
 triggers.forEach((item) => {
 	item.addEventListener("click", function () {	//на каждый вешаем событие
 		let id = this.getAttribute("data-tab");	// узнаем какой таб был нажат
 		let content = document.querySelector('.js-content[data-tab="' + id + '"]'); // и какой текст ему соответствует
+		let img = document.querySelector('.options__phone-block img[data-tab="' + id + '"]');
 
 		let active_trigger = document.querySelector(".js-trigger.active"); // находим активный в данный момент таб
 		let active_content = document.querySelector(".js-content.active"); // и текст
+		let active_img = document.querySelector(".js-img.active");
 
 		active_trigger.classList.remove("active");	//переназначаем классы с активного таба и текста на выбранные
 		item.classList.add("active");
 
 		active_content.classList.remove("active");
+		active_content.classList.remove("animate__fadeInRight"); // удаление класса анимации
 		content.classList.add("active");
+		content.classList.add("animate__fadeInRight"); // добавление класса анимации
 
-		document.querySelector(".options__phone-block img").setAttribute("src", img_arr[id - 1]); // меняем изображение на соответствующее активному табу
+		active_img.classList.remove("active");
+		active_img.classList.remove("animate__fadeInRight");
+		img.classList.add("active");
+		img.classList.add("animate__fadeInRight");
 	})
 })
